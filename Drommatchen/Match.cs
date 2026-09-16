7dfb4635229d4bcc114cfd9b2e2d61760b75d05d
Martin-Backlund <martinbacklund45@gmail.com>
2026-09-16 21:24:44 +0200

    lade till metod 2, skriv ut namn, nummer, position

--- Drommatchen/Match.cs
+++ Drommatchen/Match.cs
@@ -1,31 +1,33 @@
-public class Match
+namespace Drommatchen
 {
-    public string Hemmalag { get; private set; }
-    public string Bortalag { get; private set; }
-    public string Datum { get; private set; }
-    
-    // Pluspoäng om ni kommer på hur man kan minska ner koden med properties :)
-
-    // Konstruktor
-    public Match(string hemmalag, string bortalag, string datum)
+    public class Match
     {
-        Hemmalag = hemmalag;
-        Bortalag = bortalag;
-        Datum = datum;
+        public string Hemmalag { get; private set; }
+        public string Bortalag { get; private set; }
+        public string Datum { get; private set; }
+
+        // Pluspoäng om ni kommer på hur man kan minska ner koden med properties :)
+
+        // Konstruktor
+        public Match(string hemmalag, string bortalag, string datum)
+        {
+            Hemmalag = hemmalag;
+            Bortalag = bortalag;
+            Datum = datum;
+        }
+
+        // Metod 1: skriv ut matchens lag och datum
+        public void Presentera()
+        {
+            Console.WriteLine($"Matchens lag: {Hemmalag} och {Bortalag}");
+            Console.WriteLine($"Datum: {Datum}");
+        }
+
+        // Metod 2: skriv ut spelarens namn, nummer och position
+        public void AnnounceraMålskytt(Spelare spelare)
+        {
+            Console.WriteLine($"Namn: {spelare.Namn} Nummer: {spelare.Nummer} Position: {spelare.Position}");
+        }
     }
 
-    // Metod 1: skriv ut matchens lag och datum
-    public void Presentera()
-    {
-        Console.WriteLine($"Matchens lag: {Hemmalag} och {Bortalag}");
-        Console.WriteLine($"Datum: {Datum}");
-    }
-
-    // Metod 2: skriv ut spelarens namn, nummer och position
-    public void AnnounceraMålskytt(Spelare spelare)
-    {
-        // TODO
-    }
 }
-
-
