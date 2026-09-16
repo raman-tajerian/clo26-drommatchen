30bc2aa54b6b561b1b7ab4f104c042d124c4419d
Raman Tajerian <ramantajerian@gmail.com>
2026-09-16 21:41:31 +0200

    Ändrat spelare cs till auto-properties

--- Drommatchen/Spelare.cs
+++ Drommatchen/Spelare.cs
@@ -3,46 +3,21 @@ namespace Drommatchen
     public class Spelare
     {
 
-        // Privata fält
-        private string _namn;
-        private int _nummer;
-        private string _position;
+        // Auto-properties med publik get och privat set
+        public string Namn { get; private set; }
+        public int Nummer { get; private set;  }
+        public string Position { get; private set; }
+        public int Mal { get; private set; }
+        
 
-        private int _mal;
-
-        // Properties — publik get, privat set
-        public string Namn
-        {
-            get { return _namn; }
-            private set { _namn = value; }
-        }
-
-        public int Nummer
-        {
-            get { return _nummer; }
-            private set { _nummer = value; }
-        }
-        public string Position
-        {
-            get { return _position; }
-            private set { _position = value; }
-        }
-        public int Mal
-        {
-            get { return _mal; }
-            private set { _mal = value; }
-        }
-
-        // Konstruktor
+        // Konstruktorer
         public Spelare(string namn, int nummer, string position, int mal)
         {
-            _namn = namn;
-            _nummer = nummer;
-            _position = position;
-            _mal = mal;
-
+            Namn = namn;
+            Nummer = nummer;
+            Position = position;
+            Mal = mal;
         }
 
     }
 }
-
