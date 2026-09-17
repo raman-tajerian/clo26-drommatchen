1e7b4014fa82db228a93b117440af1fe619e06bc
Raman Tajerian <ramantajerian@gmail.com>
2026-09-17 10:56:33 +0200

    Skapar matchen, spelare, namn, nummer, position och mål i program cs

--- Drommatchen/Program.cs
+++ Drommatchen/Program.cs
@@ -1,3 +1,41 @@
+using System;
+
+namespace Drommatchen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Skapar matchen
+            Match drommatchen = new Match("FC Barcelona", "Real Madrid", "2009-05-02");
+
+            // Skapar två spelare med namn, nummer, antal mål och position
+            Spelare messi = new Spelare("Lionel Messi", 10, "Forward", 2);
+            Spelare raul = new Spelare("Raul Gonzalez", 7, "Forward", 0);
+
+
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // TODO: Skapa minst 2 spelare
 // Spelare spelare1 = new Spelare("Namn", nummer, "Position");
 // Spelare spelare2 = new Spelare(...);
